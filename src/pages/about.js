12c1c50f2ec0327002d23e0de65be31b26f105ab
Chris Honda <christopher.honda92@gmail.com>
2018-09-26 15:15:18 -0700

    Aded about and contact pages, added layout component and minimal style

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+
+export default () => (
+    <Layout>
+        <div>
+            <h1>About Me</h1>
+            <p>Life is Good.</p>
+        </div>
+    </Layout>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <Layout>
+        <div>
+            <h1>Hi! I'm building a Gatsby tutorial project!</h1>
+            <p>
+                What do I like to do?
+                I like to do things.
+            </p>
+        </div>
+    </Layout>
+)
